c4c898f42d9640ed96d5d8918212061f1ab4ad66
Pyload901 <00060021@uca.edu.sv>
2022-11-02 12:18:09 -0600

    add: validaciones para subir un elemento del menu

--- Server/routes/api/menu.router.js
+++ Server/routes/api/menu.router.js
@@ -3,8 +3,18 @@ const router = express.Router();
 
 const menuController = require('../../controllers/menu.controller');
 
+const menuValidations = require('../../validators/menu.validator');
+const runValidations = require('../../validators/index.validator');
+
 router.get('/:dia([A-z]+)', menuController.getAll);
+
 router.get('/:id', menuController.getById);
+
 router.get('/:dia/:categoria', menuController.getByCategory);
-router.post('/', menuController.save);
+
+router.post('/',
+    menuValidations.create,
+    runValidations,
+    menuController.create
+);
 module.exports = router;
