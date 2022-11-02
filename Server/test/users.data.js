6c5e4653131f2df71b58adfc74a2ba14d14b22d6
Pyload901 <00060021@uca.edu.sv>
2022-11-02 14:40:18 -0600

    add: funcionalidad para login y registro simple

---
+++ Server/test/users.data.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        _id: 1,
+        username: "David",
+        email: "daviddiaz664@gmail.com",
+        password: "$2b$10$IHXBDgHb9G3Yubl1ZEmD8uOGZCtmmHjSgCLBzzuk8MYw1RO0i7udq",
+        phone: "50376079410",
+        photo: "user.png",
+        rol: "ROL_USER",
+        institucion: "UCA"
+    },
+    {
+        _id: 2,
+        username: "root",
+        email: "root@root.com",
+        password:"$2b$10$IHXBDgHb9G3Yubl1ZEmD8uUMkpbhbkfMkg8unnVJf34aGJGVc6nYq",
+        phone: "50376079410",
+        photo: "user.png",
+        rol: "ROL_ADMIN",
+        institucion: "UCA"
+    },
+    {
+        _id: 3,
+        username: "test",
+        email: "test@test.com",
+        password: "$2b$10$IHXBDgHb9G3Yubl1ZEmD8u7snZnEOeA0Q4j.5CCZkQ7/tIJ2jU2sK",
+        phone: "50376079410",
+        photo: "user.png",
+        rol: "ROL_USER",
+        institucion: "UCA"
+    }
+];
