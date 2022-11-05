9d6dc665329be2c2496394f9ce3adb2a8b52e37b
Pyload901 <00060021@uca.edu.sv>
2022-11-04 20:16:12 -0400

    add: rutas principales para categorias

--- Server/routes/api/index.router.js
+++ Server/routes/api/index.router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const menuRouter = require('./menu.router');
+const cateogryRouter = require('./category.router');
 
 router.use('/menu', menuRouter);
+router.use('/category', cateogryRouter);
 
 module.exports = router;
