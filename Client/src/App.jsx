195e38f54bf103224cce4a5a062e847dce10ceb1
FranciscoRosa-UCA <00046821@uca.edu.sv>
2022-11-08 14:44:42 -0600

    Add: Menu - with cards and banner

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import MainHeader from './Components/MainHeader/MainHeader'
 import AccountHeader from './Components/AccountHeader/AccountHeader'
+import Container from './Components/Container/Container'
 
 function App() {
 
@@ -13,6 +14,9 @@ function App() {
       <AccountHeader>
 
       </AccountHeader>
+      <Container>
+
+      </Container>
     </div>
   )
 }
