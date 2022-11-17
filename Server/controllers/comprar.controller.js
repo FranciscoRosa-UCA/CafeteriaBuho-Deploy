f74df797138b6ec6939bbe77259d97f3945f5e08
Pyload901 <00060021@uca.edu.sv>
2022-11-17 12:07:27 -0500

    add: funciones utiles

--- Server/controllers/comprar.controller.js
+++ Server/controllers/comprar.controller.js
@@ -1,15 +1,15 @@
 const debug = require('debug')('app:compra-controller');
 const comprarController = {};
 const Compra = require('../models/Compra.model');
+const Wallet = require('../models/Wallet.model');
+const { message } = require('../utils/utils');
 
 comprarController.comprar = async (req, res) => {
     let compra = new Compra(req.body);
-    try { 
+    try {
+
         const _compra = await compra.save();
-        return res.status(200).json({
-                ok: true,
-                message: "Se ha realizado la compra existosamente"
-            });
+        return res.status(200).json(message(true, "Se ha realizado la compra con éxito"));
     } catch(e) {
         debug(e);
         return res.status(500).json({
