cfe36fba7a6675b214c876dc7980f16565e44670
Pyload901 <00060021@uca.edu.sv>
2022-11-20 22:53:07 -0500

    add: compra con wallet

--- Server/controllers/comprar.controller.js
+++ Server/controllers/comprar.controller.js
@@ -1,11 +1,27 @@
 const debug = require('debug')('app:compra-controller');
 const comprarController = {};
+const { response } = require('express');
 const Compra = require('../models/Compra.model');
-const Wallet = require('../models/Wallet.model');
+const Product = require('../models/Product.model');
+const User = require('../models/User.model');
 const { message } = require('../utils/utils');
 
 comprarController.comprar = async (req, res) => {
-    let compra = new Compra(req.body);
+    let
+    {
+        email,
+        productos
+    } = req.body;
+    const _productos = productos.map(id => Product.find({id}));
+    let usuario = User.find({email});
+    const total = _productos.reduce((acc, producto) => {
+        return acc+producto.precio;
+    }, 0.00);
+
+    if (total > usuario.wallet.ucoins) {
+        return message(false, 'No cuenta con suficientes fondos para realizar la compra');
+    }
+    let compra = new Compra({email, productos, total});
     try {
 
         const _compra = await compra.save();
