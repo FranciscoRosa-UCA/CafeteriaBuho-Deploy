c6a186163a597fb8ac6c835548b04e66060d0c86
Pyload901 <00060021@uca.edu.sv>
2022-11-21 10:49:38 -0600

    add

--- Server/routes/api/index.router.js
+++ Server/routes/api/index.router.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const userRouter = require('./user.router');
 router.use('/user', userRouter);
 
-const categoryRouter = require('./category.router');
+const categoriaRouter = require('./categoria.router');
 const productRouter = require('./product.router');
 const comprarRouter = require('./comprar.router');
 
 router.use('/product', productRouter);
-router.use('/category', categoryRouter);
+router.use('/categoria', categoriaRouter);
 router.use('/comprar', comprarRouter);
 
 
