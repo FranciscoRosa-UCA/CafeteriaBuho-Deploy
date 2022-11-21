113d228848dc5a208ac88c6571da25167026b8f9
Pyload901 <00060021@uca.edu.sv>
2022-11-21 10:25:01 -0600

    add: recargar wallet
    Former-commit-id: 25ded66cf9bb5c35242e7b57d9625d960ab47623

--- Server/routes/api/index.router.js
+++ Server/routes/api/index.router.js
@@ -6,11 +6,13 @@ router.use('/user', userRouter);
 const categoryRouter = require('./category.router');
 const productRouter = require('./product.router');
 const comprarRouter = require('./comprar.router');
+const walletRouter = require('./wallet.router');
 
 router.use('/product', productRouter);
 router.use('/user', productRouter);
 router.use('/category', categoryRouter);
 router.use('/comprar', comprarRouter);
+router.use('/wallet', walletRouter);
 
 
 module.exports = router;
