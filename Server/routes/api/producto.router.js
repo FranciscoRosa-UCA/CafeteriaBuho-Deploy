0c08611d69878360c6574dce60510bc7b7915310
Pyload901 <00060021@uca.edu.sv>
2022-11-21 22:27:36 -0600

    add: funcionalidades crud para producto
    Former-commit-id: 553d753dddf61bc762ddc7a74c31278f6383df9d

---
+++ Server/routes/api/producto.router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: '../uploads/' });
+const productoController = require('../../controllers/producto.controller');
+
+const productoValidators = require('../../validators/producto.validator');
+const runValidations = require('../../validators/index.validator');
+const productoSanitizer = require('../../sanitizers/producto.sanitizer');
+
+router.get('/:dia([A-z]+)', productoController.getAll);
+
+router.get('/:id', productoController.getById);
+
+router.get('/:dia/:categoria', productoController.getByCategory);
+
+router.post('/',
+    upload.single('imagen'),
+    productoValidators.create,
+    runValidations,
+    productoSanitizer.create,
+    productoController.create
+);
+
+router.patch('/',
+    upload.single('imagen'),
+    productoValidators.create,
+    runValidations,
+    productoSanitizer.update,
+    productoController.update);
+
+router.delete('/',
+    productoValidators.delete,
+    runValidations,
+    productoController.delete)
+    
+module.exports = router;
