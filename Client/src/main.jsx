e6f31d1c0ff808d5b349c4bdfc5c8b65d287aba8
Pyload901 <00060021@uca.edu.sv>
2022-11-22 23:30:31 -0600

    add: react router
    Former-commit-id: 7591ab28c1a0902fad9e9c00f4590d8c48122533

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './Components/Authentication/Login/Login';
+import Register from './Components/Authentication/Register/Register';
+import { Menu } from '@mui/material';
+import MainHeader from './Components/HeaderMain/HeaderMain';
+
+const router = createBrowserRouter([
+  // {
+  //   path: '/',
+  //   element: <><MainHeader /><Menu /></>
+  // },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <Register />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
