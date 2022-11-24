3cb62981001238bb07db1f3d2ac5fe0ec62e07de
Pyload901 <00060021@uca.edu.sv>
2022-11-24 16:51:04 -0500

    add:  registro usuario
    Former-commit-id: 747d3f7e3f1ea103830800e3033e050f3d4262d7

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Login from './Components/Authentication/Login/Login';
 import Register from './Components/Authentication/Register/Register';
-import { Menu } from '@mui/material';
+import { Container, Menu } from '@mui/material';
 import MainHeader from './Components/HeaderMain/HeaderMain';
 
 const router = createBrowserRouter([
-  // {
-  //   path: '/',
-  //   element: <><MainHeader /><Menu /></>
-  // },
+  {
+    path: '/',
+    element: <Container/>
+  },
   {
     path: "/login",
     element: <Login />
