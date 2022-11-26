c0a65e67c1ac61841f5c0a59e219bbaf593157b8
Carlos Hernández <00002721@uca.edu.sv>
2022-11-26 14:59:38 -0600

    Add: Changes on feature/carlos
    Former-commit-id: aadc468dbeddf8cef05a094fda37fb65d74e0068

---
+++ Client/src/Components/PaymentFailed/PaymentFailed.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "../Button/Button";
+import "./PaymentFailed.css";
+import Icon from "../Icon/Icon";
+
+const PaymentFailed= () => {
+    return(
+        <div className="general-container shadow-[0_0_20px_1px_rgba(0,0,0,0.3)]">
+                    <div className="components-container">
+                        <h1 className="titulo">Billetera</h1>
+                        <figure>
+                            <img src="https://res.cloudinary.com/dvbuu8u2x/image/upload/v1666536043/olympic_flag.jpg" alt="prueba" className="perfil"/>
+                        </figure>
+                        <h2 className="identificacion">00002721</h2>
+                        <div className="detalle">
+                            <p>Saldo actual: $ 100.00</p>
+                            <p>Total a pagar: $ 50.00</p>
+                            <p>¡No es posible realizar el pedido!</p>
+                        </div>
+                        <div className="interaccion">
+                            <Button>Recargar billetera</Button>
+                            <Button>Cancelar</Button>
+                        </div>
+                    </div>
+                </div>
+    );
+}
+
+export default PaymentFailed;
